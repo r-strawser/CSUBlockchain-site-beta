3e614772a6859423d9b0b266bfe24d80f0ff8df6
r-strawser <strawser.ryan@gmail.com>
2021-03-23 21:08:56 -0400

    Updates

---
+++ precache-manifest.ee194ab91a71396d3579daef2d142fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8eae29701cc7568af4",
+    "url": "/CSUBlockchain-site-beta/static/css/main.ba673162.chunk.css"
+  },
+  {
+    "revision": "1f8eae29701cc7568af4",
+    "url": "/CSUBlockchain-site-beta/static/js/main.1f8eae29.chunk.js"
+  },
+  {
+    "revision": "405acd2a05e8cd94bfdf",
+    "url": "/CSUBlockchain-site-beta/static/css/1.7333e154.chunk.css"
+  },
+  {
+    "revision": "405acd2a05e8cd94bfdf",
+    "url": "/CSUBlockchain-site-beta/static/js/1.405acd2a.chunk.js"
+  },
+  {
+    "revision": "b5c89d30e03fa464dd04",
+    "url": "/CSUBlockchain-site-beta/static/js/runtime~main.b5c89d30.js"
+  },
+  {
+    "revision": "f2cec74cb8846f2628bf515941cee7c8",
+    "url": "/CSUBlockchain-site-beta/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CSUBlockchain-site-beta/precache-manifest.ee194ab91a71396d3579daef2d142fa2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CSUBlockchain-site-beta/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
